330bc995b71e571096f20c175dfc9a5ebb99e5d5
isPeang <soonyata92@gmail.com>
2024-03-19 11:18:57 +0700

    can't Edit

--- src/dailytodo/TodoDayTwo.js
+++ src/dailytodo/TodoDayTwo.js
@@ -1,8 +1,57 @@
-import React from "react";
+import '../App.css'
+import React, { useState } from "react";
 
 function TodoDayTwo() {
+    const [todo, setTodo] = useState();
+    const [list, setList] = useState([]);
+    const [edit, setEdit] = useState();
+
+    function onCreate() {
+        if(edit) {
+            const newList = list.map((e, i) => {
+                if (i === edit) return i;
+                return e;
+            });
+            setList(newList);
+            setEdit('')
+
+        } else
+        if (todo.length) {
+            setList([...list, todo]);
+            console.log(list)
+            setTodo('')
+        }
+    }
+
+    function onEdit(index) {
+        const value = list.find((_, i) => i === index);
+        if(value) setTodo(value);
+        setEdit(index);
+    }
+    
+    function onDelete(index) {
+        const removedlist = list.filter((_, i) => i !== index);
+        setList(removedlist);
+
+    }
+
     return (
-        <h1>Hello</h1>
+        <div className="App">
+            <input
+                type="text"
+                value={todo}
+                onChange={e => setTodo(e.target.value)} />
+            {' '}
+            <button onClick={onCreate}>Create</button>
+
+            {list.map((list, index) =>
+                <p key={index}>
+                    {list}  {' '}
+                    <button onClick={() => onEdit(index)}>Edit</button> {' '}
+                    <button onClick={() => onDelete(index)}>Delete</button>
+                </p>
+            )}
+        </div>
     );
 
 };
