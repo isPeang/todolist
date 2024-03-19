e86cbbd4833495e753124c7943834e77105c326c
isPeang <soonyata92@gmail.com>
2024-03-19 10:14:59 +0700

    todoDay2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoDayTwo from './dailytodo/TodoDayTwo.js'
 
 function App() {
   return (
-    <div className="App">
-      <h1>main changes </h1>
-    </div>
+    <TodoDayTwo/>
   );
 }
 
