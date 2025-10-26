0ad1b42a17569c40ff6150094056a5f012ba113f
soonyata <soonyata92@gmail.com>
2025-10-26 16:34:37 +0700

    tesst

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import TodoDayTwo from './dailytodo/TodoDayTwo.js'
 import TodoDayThree from './dailytodo/TodoDayThree.js';
 import TodoDayFour from './dailytodo/TodoDayFour.js';
+import Todo2025_Day1 from './dailytodo/Todo2025_Day1.js';
 
 function App() {
   return (
-    <TodoDayFour/>
+    <Todo2025_Day1/>
   );
 }
 
---
+++ src/dailytodo/Todo2025_Day1.js
@@ -0,0 +1,13 @@
+function Todo2025_Day1() {
+    return (
+        <div>
+            <input placeholder="ทำอะไรดี...">
+            
+            </input>
+
+            <button>สร้าง</button>
+        </div>
+    )
+}
+
+export default Todo2025_Day1;
